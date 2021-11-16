deb036f37d4c8921eabc0c9844ef509ed05ffdf7
leossm-creator <ssangminleo@gmail.com>
2021-11-16 18:02:47 +0900

    daily: i18n

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -3,20 +3,22 @@ import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
-const messages = {
-  'en': {
-      welcomeMsg: 'Welcome to Your Vue.js App'
-  },
-  'kr': {
-      welcomeMsg: 'Vue.js App에 오신것을 환영합니다'
-  }
-};
+// const messages = {
+//   'en': {
+//       welcomeMsg: 'Welcome to Your Vue.js App'
+//   },
+//   'kr': {
+//       welcomeMsg: 'Vue.js App에 오신것을 환영합니다'
+//   }
+// };
 
 const i18n = new VueI18n({
   silentTranslationWarn: true,
   locale: 'kr', // set locale
   fallbackLocale: 'en', // set fallback locale
-  messages, // set locale messages
+  messages: {
+    dev: require('./locales/dev.json')
+  }, // set locale messages
 });
 
 export default i18n
---
+++ src/plugins/locales/locales.js
@@ -0,0 +1,16 @@
+export const SUPPORTED_LOCALES = [
+  {
+    code: 'kr',
+    base: '',
+    flag: 'fr',
+    name: 'Korean',
+    translations: '/translations/kr.json'
+  },
+  {
+  code: 'en',
+  base: '/en',
+  flag: 'us',
+  name: 'English',
+  translations: '/translations/en.json'
+  }
+]
